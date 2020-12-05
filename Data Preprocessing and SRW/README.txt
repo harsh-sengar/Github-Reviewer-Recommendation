3b5c86b478d199cfc96e082bd26118704d1b14e8
Gaurav Tank <code.tankgauravgt@gmail.com>
2020-12-05 17:45:35 +0530

    Data Processing and SRW Code Sanitized

---
+++ Data Preprocessing and SRW/README.txt
@@ -0,0 +1,5 @@
+# Unzip all the files:
+
+>> sh unzip.sh
+
+After extracting the dataset files, run the notebooks in the order.
---
+++ Data Preprocessing and SRW/unzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+unzip ./srw.zip
+unzip ./matrix.zip
+unzip ./pull_req_titles.zip
+unzip ./kubernetes.zip
+unzip ./raw.zip
+unzip ./processed.zip
+unzip ./cache.zip
+unzip ./filepath.zip
+unzip ./titles.zip
+
+mv filepath[* cache/dicts/req_dicts/filepath
+mv titles[* cache/dicts/req_dicts/titles
+
